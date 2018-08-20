d9ba2f1b7bf6a38ebd13c350a6eee7d4a20a24d0
mark <mah6@williams.edu>
2018-08-20 23:16:38 +0900

    List-based output, inner and left joins.

--- Data/Job.hs
+++ Data/Job.hs
@@ -1,43 +1,95 @@
 module Data.Job where
 
+import Control.Applicative
 import Data.Foldable
 import qualified Data.IntMap as M
+import Data.Maybe
 
-newtype Job t r a = Job { unJob :: t r -> r -> a }
+newtype Job r a = Job { unJob :: [r] -> r -> [a] }
 
-instance Functor t => Functor (Job t r) where
-    fmap f (Job j) = Job $ \rows row -> f $ j rows row
+instance Functor (Job r) where
+    fmap f (Job j) = Job $ \rows row -> f <$> j rows row
 
-instance Functor t => Applicative (Job t r) where
-    pure = Job . const . const
-    (Job fj) <*> (Job j) = Job $ \rows row -> fj rows row (j rows row)
+instance Applicative (Job r) where
+    pure x = Job $ \_ _ -> [x]
+    (Job fj) <*> (Job j) = Job $ \rows row -> fj rows row <*> j rows row
 
-instance Functor t => Monad (Job t r) where
+instance Monad (Job r) where
     return = pure
-    (Job j) >>= f = Job $ \rows row -> unJob (f (j rows row)) rows row
+    (Job j) >>= f = Job $ \rows row -> do
+        ff <- unJob . f <$> j rows row
+        ff rows row
 
-col :: (r -> a) -> Job t r a
-col f = Job $ \_ row -> f row
+instance Alternative (Job r) where
+    empty = Job $ \_ _ -> []
+    (Job j0) <|> (Job j1) = Job $ \rows row ->
+        case j0 rows row of
+            [] -> j1 rows row
+            stuff -> stuff
 
-row :: Job t r r
-row = Job $ \_ row -> row
+col :: (r -> a) -> Job r a
+col f = Job $ \_ row -> [f row]
 
-everything :: Job t r (t r)
-everything = Job const
+row :: Job r r
+row = Job $ \_ row -> [row]
 
-runJob :: Functor t => Job t r a -> t r -> t a
-runJob (Job j) rs = let f = j rs in f `seq` f <$> rs
+everything :: Job r [r]
+everything = Job $ \rows _ -> [rows]
 
-withGroups :: (Foldable t, Enum b) => (r -> b) -> Job [] r a -> Job t r a
-withGroups grouper_ (Job f) = Job $ \rows -> let groups = M.map f (mkGroups rows) in \row -> (groups M.! grouper row) row
+runJob :: Job r a -> [r] -> [a]
+runJob (Job j) rs = let f = j rs in concatMap f rs
+
+summarize :: (r -> a) -> ([a] -> b) -> Job r b
+summarize mapper summarizer = Job $ \rows _ -> return . summarizer . fmap mapper $ rows
+
+by :: ([a] -> b) -> (r -> a) -> Job r b
+by = flip summarize
+
+-- FIXME: For joins, compute key map on the smaller collection
+
+enumJoinInner :: (Enum k, Foldable t) => (r -> k) -> (s -> k) -> t s -> Job r s
+enumJoinInner primary external target = Job $ \_ row -> 
+    M.findWithDefault [] (fromEnum $ primary row) groups
+    where
+        groups = mkGroups (fromEnum . external) target
+
+enumJoinLeft :: (Enum k, Foldable t) => (r -> k) -> (s -> k) -> t s -> Job r (Maybe s)
+enumJoinLeft primary external target = Job $ \_ row ->
+    case M.lookup (fromEnum $ primary row) groups of
+        Just matches -> Just <$> matches
+        Nothing -> pure Nothing
+    where
+        groups = mkGroups (fromEnum . external) target
+
+-- | An inner join with unmatched items in a seperate list.
+-- A proper right join seems very tricky to me without some 
+-- sort of natural transformations magic.
+-- Probably very slow, don't use
+enumJoinRightFake :: (Enum k, Foldable t) => (r -> k) -> (s -> k) -> t s -> Job r (s, [s])
+enumJoinRightFake primary external target = Job $ \rows -> 
+    let pg = mkGroups p rows in \row ->
+    (flip (,) . concat . M.elems $ M.difference groups pg) <$>
+        M.findWithDefault [] (fromEnum $ primary row) groups
+    where
+        p = fromEnum . primary
+        f = fromEnum . external
+        groups = mkGroups f target
+
+withGroups :: Enum b => (r -> b) -> Job r a -> Job r a
+withGroups grouper_ (Job f) = Job $ \rows -> let groups = M.map f (mkGroups grouper rows) in \row -> (groups M.! grouper row) row
     where
         grouper = fromEnum . grouper_
-        insGroup row = M.insertWith (++) (grouper row) [row]
-        mkGroups = foldr' insGroup M.empty
 
-groupSummarize :: (Foldable t, Enum b) => (r -> b) -> (r -> c) -> ([c] -> a) -> Job t r a
-groupSummarize grouper mapper summarizer = withGroups grouper $ summarizer. fmap mapper <$> everything
+groupSummarize :: Enum b => (r -> b) -> (r -> c) -> ([c] -> a) -> Job r a
+groupSummarize grouper mapper summarizer = withGroups grouper $ summarize mapper summarizer
 
 -- Horribly slow. Don't use this.
-groupBy :: Enum b => (r -> b) -> Job [] r [r]
+groupBy :: Enum b => (r -> b) -> Job r [r]
 groupBy grouper = withGroups grouper everything
+
+-- Auxillaries
+insGroup :: (r -> M.Key) -> r -> M.IntMap [r] -> M.IntMap [r]
+insGroup f row = M.insertWith (++) (f row) [row]
+
+mkGroups :: Foldable t => (r -> M.Key) -> t r -> M.IntMap [r]
+mkGroups f = foldr' (insGroup f) M.empty
--- example.hs
+++ example.hs
@@ -1,41 +1,81 @@
+import Control.Applicative
 import Data.Foldable
-import qualified Data.Vector as V
-import Data.Job
+import Control.Monad
 
-data Gender = M | F deriving (Enum, Ord, Eq, Show)
+import Data.Job
 
 data Person = Person
     { height :: Double
     , weight :: Double
     , gender :: Gender
-    } deriving Show
+    }
 
+data Gender = M | F | Unknown deriving Enum
+
+-- Contrived Example
+heightVsWOGM :: Job Person (Double, Bool, Double, String) 
+heightVsWOGM = do
+    -- Operate on individual rows with "row"
+    person <- row
+
+    -- Filtering with Alternative instance
+    guard $ case gender person of
+        Unknown -> False
+        _ -> True
+    
+    -- By-group summaries 
+    genderAvgWeights <- withGroups gender $ do
+        person <- row
+        guard $ weight person > 0
+        mean `by` weight
+
+    -- Enum left and inner joins on any Foldable target
+    let genderNames = Just (M, "Male")
+        genderNames2 = [(M, error "Won't be used"), (F, "Female")]
+
+    -- Sane Alternative instance for failed joins
+    (_, gn) <- enumJoinInner gender fst genderNames
+        <|> enumJoinInner gender fst genderNames2
+
+    let tallFuncs = [(M,  (>=190))]
+   
+    -- Left joins are represented with a Maybe value.
+    tallFunc'm <- enumJoinLeft gender fst tallFuncs
+
+    -- No right joins yet, sorry.
+    -- Might be possible with some natural transformations magic.
+    -- For now, you have to run another job over the target.
+    let isTall = maybe (>=175) snd tallFunc'm $ height person
+    
+    -- Return data in the format you want it output.
+    return (height person, isTall, genderAvgWeights, gn)
+
+-- I wrote it specialized to Lists,
+-- but any Traversable + Alternative works.
+-- Vectors are super slow due to repeated concats.
+main = mapM_ print res
+    where res = runJob heightVsWOGM people
+
+-- Small example.
 people :: [Person]
-people = [Person 180 72 M, Person 160 50 F, Person 175 60 F, Person 200 100 M, Person 175 68 M]
+people = [Person 180 72 M, Person 160 50 F, Person 175 60 F, Person 200 100 M, Person 175 68 M, Person 9001 9001 Unknown]
+
+-- Bigger example.
+-- DO NOT RUN WITHOUT AT LEAST -O.
+-- Not fast, but surprisingly not slow!
+-- Processes the below in 1.722s with -O2.
+main' = mapM_ print $ take 32 res
+    where res = runJob heightVsWOGM people
 
 people2 :: [Person]
 people2 = do
-    h <- [1..100]
-    w <- [1..100]
+    h <- [1..1000]
+    w <- [1..1000]
     g <- [M, F, F, M]
     return $ Person h w g
 
--- Person's height vs. their difference from their gender's mean weight
-heightVsWOGM :: Traversable t => Job t Person (Double, Double) 
-heightVsWOGM = do
-    h <- col height
-    w <- col weight
-
-    genderAvgWeights <- withGroups gender $ do
-        ev <- everything
-        return . mean . fmap weight $ ev
-
-    return (h, w / genderAvgWeights)
-
-mean xs = sum xs / fromIntegral (length xs)
-
-vp = V.fromList people2
-
-pipeline = [people2, people, people2]
-
-main = mapM_ print $ runJob heightVsWOGM people2
+mean :: Fractional a => [a] -> a
+mean xs = s' / l'
+    where
+        (s', l') = foldl go (0, 0) xs
+        go (s, l) x = s `seq` l `seq` (s + x, l + 1)
